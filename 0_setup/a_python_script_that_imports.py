2163e1d1f469d08914ce73e694fa751141ba2952
lpeveHA <lpeve01@gmail.com>
2022-10-17 20:59:26 -0500

    Update a_python_script_that_imports.py

--- 0_setup/a_python_script_that_imports.py
+++ 0_setup/a_python_script_that_imports.py
@@ -6,5 +6,5 @@ import a_python_module
 # Running functions and create an instance of HAEmployee
 print(a_python_module.adding_two_numbers(4, 5))
 print(f"Today's date and time is: {a_python_module.get_todays_date()}")
-lorenzo = a_python_module.HAEmployee('Lorenzo Peve', 26, knows_Python = False)
+lorenzo = a_python_module.Employee('Lorenzo Peve', 26, knows_Python = False)
 print(lorenzo)
